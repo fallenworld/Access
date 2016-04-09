cdf0cb58c553bb17dd8551d78de781989dffbced
fallenworld <fallenworlder@gmail.com>
2016-04-09 17:32:38 +0800

    Commands test

--- access_commands.c
+++ access_commands.c
@@ -1,12 +1,46 @@
 #include "access_commands.h"
+#include <stdio.h>
 
 #ifdef TARGET_RASPBERRY
 
-#include <wiringpi.h>
+#include <wiringPi.h>
 int is_setup = 0;
 #define ACCESS_PIN 15
 
 #endif
+
+int writeFile(int state)
+{
+    FILE* fp = fopen("access_state", "wb+");
+    if (fp == NULL)
+    {
+        puts("Cannot open file access_state");
+    }
+    int ret = fwrite(&state, sizeof(int), 1, fp);
+    if(ret <= 0)
+    {
+        puts("Cannot write file access_state");
+    }
+    fclose(fd);
+    return ret;
+}
+
+int readFile()
+{
+    int state
+    FILE* fp = fopen("access_state", "rb");
+    if (fp == NULL)
+    {
+        puts("Cannot open file access_state");
+    }
+    int ret = fread(&state, sizeof(int), 1, fp);
+    if(ret <= 0)
+    {
+        puts("Cannot read file access_state");
+    }
+    fclose(fd);
+    return ret;
+}
 
 int openDoor()
 {
@@ -19,6 +53,7 @@ int openDoor()
         is_setup = 1;
     }
     digitalWrite(ACCESS_PIN, LOW);
+    writeFile(1);
     if (getDoorState() == 1)
     {
         return 0;
@@ -44,6 +79,7 @@ int closeDoor()
         is_setup = 1;
     }
     digitalWrite(ACCESS_PIN, HIGH);
+    writeFile(0);
     if (getDoorState() == 0)
     {
         return 0;
@@ -62,8 +98,15 @@ int getDoorState()
 {
     puts("Executing getDoorState");
 #ifdef TARGET_RASPBERRY
-    // TODO
-    return -2;
+    int state = readFile();
+    if (state != 0 && state != 1)
+    {
+        return -2
+    }
+    else
+    {
+        return state;
+    }
 #else
     return 0;
 #endif
